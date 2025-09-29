7bcc5d8ce791493ab272858bd29227424f7f3d05
aldaaunillah-cloud <aldaaunillah59@gmail.com>
2025-09-29 10:24:33 +0700

    praktikum 3(2403050)

---
+++ praktikum 3/latihan-package-import/model/Mahasiswa.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Mahasiswa {
+
+    public double ipk;
+    
+}
+
+    
+    
---
+++ praktikum 3/latihan-package-import/view/MahasiswaView.java
@@ -0,0 +1,19 @@
+package view;
+
+import model.Mahasiswa;
+public class MahasiswaView{
+
+
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.ipk = 3.8;
+        
+    }
+
+}
+
+
+
+    
+    
+
